c451695dc14cca8adca4786e5aaafd6b468666a9
ngzdllo <ngzdllo@gmail.com>
2022-11-04 08:57:18 -0500

    se empieza el menu de habitaciones

---
+++ clientehola/src/app/componente1/componente1.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente1',
+  templateUrl: './componente1.component.html',
+  styleUrls: ['./componente1.component.css']
+})
+export class Componente1Component {
+
+  constructor() { }
+
+
+
+}
